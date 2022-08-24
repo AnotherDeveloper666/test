27b0b6ce3c3e81b0da9cd2136b5dbf6459ef90dd
Marcos Anzurez <programacion.ti@telnycs.com>
2022-08-24 16:42:47 -0500

    Cambios finales

--- archivo2.js
+++
@@ -1,5 +0,0 @@
-console.log("chanchito feliz");
-console.log("chanchito triste");
-const constante = "constante";
-// ESTE ES UN CAMBIO PENDIENTE EN MASTER BRANCH
-const chanchito = "feliz";
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
+// THIS IS A TEST
 function Test(){
     console.log("This is a test function");
-    console.log("Hola Mundo");
 }
 
 Test();
